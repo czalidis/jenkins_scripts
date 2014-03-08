8ed03937ca2a0e641cbd55c7576f1190cc66d811
Chris Zalidis <zalidis@gmail.com>
2014-03-08 16:13:58 +0200

    identify repo name automatically

--- generate_documentation.sh
+++ generate_documentation.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+REPO_DIR=upstream
+export REPO_NAME="$(git remote -v | head -n1 | awk '{print $2}' | sed 's/.*\///' | sed 's/\.git//')"
 export REPOS_FILE=$JENKINS_HOME/repos_data/repos.yaml
 export PUBLIC_DIR=$JENKINS_HOME/public_html/api
 
-source /opt/ros/hydro/setup.bash
+mkdir $REPO_DIR
 
-$WORKSPACE/jenkins_scripts/generate_doc.py $REPO_NAME $WORKSPACE/doc 
+find . -maxdepth 1 -mindepth 1 -not -name $JENKINS_SCRIPTS -exec mv '{}' $REPO_DIR \;
+
+source /opt/ros/hydro/setup.bash
+$WORKSPACE/jenkins_scripts/generate_doc.py $REPO_DIR $WORKSPACE/doc 
